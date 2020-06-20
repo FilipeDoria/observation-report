0d858cde54d8c7b780812d7dbb5694c793f4910e
DiligentDev <thediligentdev@gmail.com>
2020-06-20 11:37:40 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-formulate />
+    <vue-form-generator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueFormulate from './components/VueFormulate';
+import VueFormGenerator from './components/VueFormGenerator';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueFormulate,
+    VueFormGenerator    
   }
 }
 </script>
@@ -21,8 +23,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 10vh 20vw;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './assets/formulate.css'
+import VueFormulate from '@braid/vue-formulate'
+
+import VueFormGenerator from "vue-form-generator";
+import "vue-form-generator/dist/vfg-core.css";
+
+Vue.use(VueFormGenerator);
+
+Vue.use(VueFormulate)
+
+
 Vue.config.productionTip = false
 
 new Vue({
